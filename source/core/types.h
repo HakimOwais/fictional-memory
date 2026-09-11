a63cb88c62fd593fe4e97c38eabe000dbc040ff5
HakimOwais <owaisibnmushtaq@gmail.com>
2026-09-11 10:34:47 +0530

    order types added

--- source/core/types.h
+++ source/core/types.h
@@ -20,5 +20,26 @@ using Timestamp = std::uint64_t;
 
 using ParticipantId = std::uint32_t;
 
+using MatchId = std::uint64_t;
+
+inline constexpr Price PRICE_SCALE = 10'000;
+
+enum class Side : std::uint8_t {
+    BUY,
+    SELL,
+};
+
+enum class OrderType: std::uint8_t {
+    LIMIT,
+    MARKET,
+    IOC,  // Immediate or Cancel order -- it fills what it can
+    FOK, // fill or kill --- no partial trades (all now or nothing)
+    GTC, // 
+    STOP,
+    STOP_LIMIT,
+    ICEBERG, // hidden reserve when trade in masses
+    POST_ONLY,
+    
+};
 
 }
